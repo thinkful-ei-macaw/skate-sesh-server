4dad34f76cdd9dc51428ad4da6b2e690756228cd
Raul Garcia <punkvomit21@gmail.com>
2020-04-14 22:03:15 -0400

    seeded dummy data

---
+++ seeds/seed.skatesesh_logs.sql
@@ -0,0 +1,6 @@
+INSERT INTO skatesesh (id, board, notes)
+VALUES
+(1,'Foundation 8.0', 'Had a great session! working on kickflip' ),
+(2,'Zero 7.75', 'practicing my grabs, and fakie tre-flip'),
+(3,'Foundation 8.0', 'Missed this board, got nollie shov down'),
+(4,'Arbor longboard', 'just cruised by the beach');
